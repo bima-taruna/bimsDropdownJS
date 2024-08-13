25cea5122480ffcd3f9c70e78b0a68d4e9f53dc5
bima-taruna <bimataruna21@gmail.com>
2024-08-13 18:01:54 +0700

    create dropdown class and basic method

---
+++ src/component/dropdown.js
@@ -0,0 +1,20 @@
+class Dropdown {
+  constructor() {
+    this.body = document.createElement("div");
+    this.body.classList.add("dropdown-body");
+  }
+
+  appendToParent(parent) {
+    parent.appendChild(this.body);
+  }
+
+  removeFromParent(parent) {
+    for (let i = 0; i < parent.children.length; i++) {
+      if (parent.children[i].className === "dropdown-body") {
+        parent.removeChild(parent.children[i]);
+      }
+    }
+  }
+}
+
+export default Dropdown;
