c368a73f8d3843fb96a87ca46e09da19bab1670a
bima-taruna <bimataruna21@gmail.com>
2024-08-13 18:01:36 +0700

    implement dropdown to test button

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import "./style/index.css";
+import Dropdown from "./component/dropdown";
 
 if (process.env.NODE_ENV !== "production") {
   console.log("Looks like we are in development mode!");
 }
+
+(function () {
+  const content = document.getElementById("content");
+  const testButton = document.getElementById("dropdown-test-button");
+  testButton.addEventListener("click", () => {
+    const newDropdown = new Dropdown();
+    if (!testButton.classList.contains("activated")) {
+      testButton.classList.add("activated");
+      newDropdown.appendToParent(content);
+    } else {
+      testButton.classList.remove("activated");
+      newDropdown.removeFromParent(content);
+    }
+  });
+})();
